9bf3475ffcee0d881602b7546f5d11a8c60d70e2
Nikki Daly <nicoledaly704@gmail.com>
2020-06-25 17:18:25 +0000

    Finished even or odd coding challenge

---
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/EvenOdd.java
@@ -0,0 +1,26 @@
+package ca.jrvs.practice.codingChallenge;
+
+/**
+ * ticket: https://www.notion.so/Sample-Check-if-a-number-is-even-or-odd-234dd289a4154a5fba8831d7bc569b91
+ */
+public class EvenOdd {
+
+  /**
+   * Description : Determine if the number is even or odd using modulo
+   * Big O : O(1)
+   * Justification : It's a single modulo operation
+   */
+  public String evenOddMod(int i) {
+    return i % 2 == 0 ? "Even" : "Odd";
+  }
+
+  /**
+   * Description : Determine if the number is even or odd using bit operationa
+   * Big O : O(1)
+   * Justification : It's a single XOR operation
+   */
+  public String evenOddBit(int i) {
+    return (i ^ 1) == (i + 1) ? "Even" : "Odd";
+  }
+
+}
